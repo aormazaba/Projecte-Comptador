c0e59355f6d1c2c233e3b3ce05311e8bdb7fd32e
aormazaba <aormazaba@iesebre.com>
2021-10-11 20:09:15 +0200

    Versió on incrementem el comptador

--- app/src/main/java/me/albertormazabal/dam/comptador/MainActivity.kt
+++ app/src/main/java/me/albertormazabal/dam/comptador/MainActivity.kt
@@ -2,10 +2,37 @@ package me.albertormazabal.dam.comptador
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    internal lateinit var tapMeButton: Button
+    internal lateinit var timeTextView: TextView
+    internal lateinit var counterTextView: TextView
+    internal var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tapMeButton = findViewById(R.id.tapMeButton)
+        timeTextView = findViewById(R.id.timeTextView)
+        counterTextView = findViewById(R.id.counterTextView)
+
+        //TODO haurem d'executar incrementCounter
+        // Actualitzarem valor inicial del counterTextView
+
+        tapMeButton.setOnClickListener{
+            incrementCounter()
+
+        }
+    }
+
+    private fun incrementCounter() {
+        counter += 1
+        counterTextView.text = counter.toString()
+
+
     }
 }
