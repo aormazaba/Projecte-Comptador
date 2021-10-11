251e917903731c7ead694d09200eac129a3cb125
aormazaba <aormazaba@iesebre.com>
2021-10-11 21:08:06 +0200

    Versió que se reinicia al finalitzar

--- app/src/main/java/me/albertormazabal/dam/comptador/MainActivity.kt
+++ app/src/main/java/me/albertormazabal/dam/comptador/MainActivity.kt
@@ -13,11 +13,11 @@ class MainActivity : AppCompatActivity() {
     internal lateinit var timeTextView: TextView
     internal lateinit var counterTextView: TextView
     internal var counter = 0
-    internal var time = 60
+    internal var time = 10
 
     internal var appStarted = false
     internal lateinit var countdownTimer : CountDownTimer
-    internal val initialCountDownTimer: Long = 60000
+    internal val initialCountDownTimer: Long = 10000
     internal val intervalCountDownTimer: Long = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -75,6 +75,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun resetGame() {
-        
+        counter = 0
+        counterTextView.text = counter.toString()
+        timeTextView.text = getString(R.string.timeText, time)
+        appStarted = false
+        tapMeButton.setOnClickListener{
+            if (!appStarted){
+                startGame()
+                countdownTimer.start()
+                appStarted = true
+            }
+            incrementCounter()
+
+        }
+
     }
 }
